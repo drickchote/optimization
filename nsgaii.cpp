678cc90d5094adbe93cc2bc9ca761708271ee3e9
Marcos Adriano <mdrick19@gmail.com>
2026-01-06 22:22:44 -0300

    add main loop

--- nsgaii.cpp
+++ nsgaii.cpp
@@ -5,25 +5,52 @@
 using namespace std;
 
 
+static constexpr int POP_SIZE = 40;
+static constexpr int GENERATIONS = 500;
+static constexpr int NUMBER_OF_ASSETS = 31; // get from the file portx.txt
+static constexpr double LB = 0.01; //
+static constexpr double UB = 1.0; // 
+static constexpr double K = 10; // max picked assets 
+
+
+
 // max return
 // min risk
 
 struct Individual{
-    double expectedReturn;
-    double risk;
+    vector<bool> picked; // list of assets weights
+    vector<double> weights; // list of assets weights
+    double expectedReturn; // maximize 
+    double risk; // minimize
     int rank;
     int dominatedCount;
-    list<Individual> dominates;
+    vector<Individual> dominates;
 };
 
-list<Individual> generate_population(){
+vector<double> generateDecision(int size){
 
 }
+
+vector<Individual> generate_population(int size){ // generates a random population
+    vector<Individual> population;
+    population.reserve(POP_SIZE);
+
+    for(int i=0; i<size; i++){
+        Individual individual = Individual();
+        
+    }
+}
+
+vector<Individual> generate_population(vector<Individual> population){ // generates a population with genetic operations
+
+}
+
 
 bool dominates(const Individual& individual1, const Individual& individual2){
-    return (individual1.expectedReturn > individual2.expectedReturn &&  individual1.risk >= individual2.risk)
-            || (individual1.expectedReturn >= individual2.expectedReturn &&  individual1.risk > individual2.risk);
+    return (individual1.expectedReturn > individual2.expectedReturn &&  individual1.risk <= individual2.risk)
+            || (individual1.expectedReturn >= individual2.expectedReturn &&  individual1.risk < individual2.risk);
 }
+
 
 void evaluate(list<Individual> &population){
     for(auto individual1 : population){
@@ -39,15 +66,52 @@ void evaluate(list<Individual> &population){
     }
 }
 
+vector<vector<Individual>> non_dominant_sort(vector<Individual> population){
+
+}
+
+void sort(vector<Individual> &population){ // sort by crowding distance
+
+}
+
+/**
+ * Do operations to ensure the constraints
+ */
+void repairPopulation(vector<Individual> &population){ 
+
+}
+
 
 
 int main(){
-    list<Individual> population = generate_population();
+    vector<Individual> population = generate_population(POP_SIZE);
+    population.reserve(POP_SIZE * 2);
 
+    int generation = 0;
     
-    for(auto individual: population){
-        if(individual.dominatedCount == 0){
+    vector<Individual> nextGeneration;
+    nextGeneration.reserve(population.size());
+    
+    for(int i=0; i<GENERATIONS; i++){
+        vector<vector<Individual>> frontiers = non_dominant_sort(population); 
 
+        int frontierIndex=0;
+        while(nextGeneration.size() + frontiers[frontierIndex].size() < POP_SIZE){
+            nextGeneration.insert(nextGeneration.end(), frontiers[frontierIndex].begin(), frontiers[frontierIndex].end());
+            frontierIndex++;
         }
+        sort(frontiers[frontierIndex]);
+        int remainingSpace =  POP_SIZE-nextGeneration.size();
+        nextGeneration.insert(nextGeneration.begin(), frontiers[frontierIndex].begin(), frontiers[frontierIndex].begin() + remainingSpace);
+
+        vector<Individual> offspring = generate_population(nextGeneration);
+        repairPopulation(offspring);
+
+        // concatenation of nextGeneration + offspring
+        population.clear();
+        population.insert(population.end(), nextGeneration.begin(), nextGeneration.end());
+        population.insert(population.end(), offspring.begin(), offspring.end());
     }
+    
+   
 }
